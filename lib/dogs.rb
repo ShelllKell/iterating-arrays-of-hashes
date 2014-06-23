96193c825cf0c52c7c2a71b03d4ceb829f7f60f2
Shelby Kelly <shelby.kelly@colorado.edu>
2014-06-23 16:41:56 -0600

    updated dogs file

--- lib/dogs.rb
+++ lib/dogs.rb
@@ -26,8 +26,120 @@ class Dogs
       {name: "Harleigh", size: :medium, owner: andrew},
       {name: "Trixie",   size: :small,  owner: andrew}
     ]
-  end
+    end
+
 
   # only edit below this line
+
+  def small_dogs
+
+    @dogs.select { |dog| dog[:size] == :small}
+
+  #   small_dogs = []
+  #   @dogs.each do |dog|
+  #     if dog[:size] == :small
+  #       small_dogs.push({name: dog[:name]})
+  #     end
+  #   end
+  # small_dogs
+  end
+
+  def huge_dog
+    huge_dogs = []
+    @dogs.each do |dog|
+      if dog[:size] == :huge
+        huge_dogs.push({name: dog[:name]})
+      end
+    end
+    huge_dogs
+  end
+
+  def large_dog_names
+    large_dogs = []
+    @dogs.each do |dog|
+      if dog[:size] == :large
+        large_dogs.push(dog[:name])
+      end
+    end
+    large_dogs
+  end
+
+  def joes_large_dogs
+    large_dogs = []
+    @dogs.each do |dog|
+
+      if dog[:owner][:name][:first] == "Joe"
+        if dog[:size] == :large
+            large_dogs.push(dog[:name])
+        end
+      end
+     end
+    large_dogs
+  end
+
+  def sizes
+    sizes = []
+    @dogs.each do |dog|
+     sizes.push(dog[:size])
+    end
+    sizes.uniq
+
+  end
+
+  def owners
+    owners = []
+    @dogs.each do |dog|
+      owners.push(dog[:owner][:name][:first] +" "+ dog[:owner][:name][:last])
+    end
+     owners.uniq
+  end
+
+  def average_owners
+    avg_owners = []
+    @dogs.each do |dog|
+      if dog[:owner][:owner_quality] == AVERAGE
+        avg_owners.push(dog[:owner][:name][:first] +" "+ dog[:owner][:name][:last])
+      end
+
+    end
+    avg_owners.uniq
+  end
+
+  def to_s
+    owners = [[],[],[]]
+    @dogs.each do |dog|
+      if dog[:owner][:name][:first] == "Joe"
+        owners[0].push(dog[:name])
+      elsif dog[:owner][:name][:first] == "Sarah"
+        owners[1].push(dog[:name])
+      else
+        owners[2].push(dog[:name])
+      end
+    end
+    str = "Joe owns: #{owners[0][0]}, #{owners[0][1]}, and #{owners[0][2]}\nSarah owns: #{owners[1][0]} and #{owners[1][1]}\nAndrew owns: #{owners[2][0]} and #{owners[2][1]}"
+
+  end
+
+  def find_by_owner(name)
+
+      find_owner = []
+      @dogs.each do |dog|
+        owner = dog[:owner][:name].values
+        p " ??????"
+        if dog[:owner][:name][:first] == name
+          find_owner.push({name: dog[:name]})
+        elsif dog[:owner][:name][:last] == name
+          find_owner.push({name: dog[:name]})
+        elsif owner[0] + " " + owner[1] == name
+          find_owner.push({name: dog[:name]})
+          p "<<<<<<"
+        end
+      end
+      p "  jj"
+    p find_owner
+
+  end
+
+
 
 end
